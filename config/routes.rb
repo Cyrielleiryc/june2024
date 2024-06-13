a7ba871e74bedcc721b98876eb281b95f498b865
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2024-06-13 20:50:20 +0200

    added routes for books API

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :books, only: %i[index show create update destroy]
+    end
+  end
 end
